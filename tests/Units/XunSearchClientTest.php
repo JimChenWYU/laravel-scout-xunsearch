b847abb8ea83ccc8e7a34ad3e3f658920bb3c01d
JimChen <imjimchen@163.com>
2021-04-29 16:52:41 +0800

    wip: add tests

---
+++ tests/Units/XunSearchClientTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Tests\Units;
+
+use JimChen\LaravelScout\XunSearch\Builders\TokenizerBuilder;
+use JimChen\LaravelScout\XunSearch\Tokenizers\Results\Top;
+use JimChen\LaravelScout\XunSearch\XunSearchClient;
+use SplFixedArray;
+use stdClass;
+use Tests\TestCase;
+use Mockery as m;
+
+class XunSearchClientTest extends TestCase
+{
+    public function test_participle()
+    {
+        $builder = m::mock(TokenizerBuilder::class);
+        $builder->shouldReceive('withXs')->withAnyArgs()->andReturnSelf();
+        $builder->shouldReceive('build')->withAnyArgs()->andReturnSelf();
+        $builder->shouldReceive('throughMiddleware')->withAnyArgs()->andReturnSelf();
+        $builder->shouldReceive('getTops')->with(
+            'foobar', 5, 'n,nr,ns,nz,v,vn'
+        )->andReturn(SplFixedArray::fromArray([
+            new Top(1, '', 'foo'),
+            new Top(1, '', 'bar'),
+        ]));
+
+        $client = new XunSearchClient(
+            'localhost:8383',
+            'localhost:8384',
+            $builder,
+            'uft8',
+            [
+                'schemas' => [
+                    'user' => [
+                        'id' => [
+                            'type' => 'id',
+                        ],
+                    ],
+                ],
+            ]
+        );
+
+        self::assertEquals([
+            'foo', 'bar'
+        ], $client->participle('user', 'foobar'));
+    }
+
+    public function test_loadconfig()
+    {
+        $builder = m::mock(TokenizerBuilder::class);
+
+        $client = new XunSearchClient(
+            'localhost:8383',
+            'localhost:8384',
+            $builder,
+            'gbk',
+            [
+                'schemas' => [
+                    'user' => [
+                        'id' => [
+                            'type' => 'id',
+                        ],
+                    ],
+                ],
+            ]
+        );
+
+        self::assertEquals([
+            'server.search' => 'localhost:8384',
+            'server.index' => 'localhost:8383',
+            'project.default_charset' => 'gbk',
+            'project.name' => 'user',
+            'id' => [
+                'type' => 'id'
+            ],
+        ], $client->loadConfig('user'));
+    }
+
+    public function test_single_instance_xunsearch_init()
+    {
+        $builder = m::mock(TokenizerBuilder::class);
+        $client = new TestXunSearchClient(
+            'localhost:8383',
+            'localhost:8384',
+            $builder,
+            'gbk',
+            [
+                'schemas' => [
+                    'user' => [
+                        'id' => [
+                            'type' => 'id',
+                        ],
+                    ],
+                    'post' => [
+                        'id' => [
+                            'type' => 'id',
+                        ],
+                    ],
+                ],
+            ]
+        );
+
+        $o1 = $client->testInitXunSearch('user');
+        $o2 = $client->testInitXunSearch('user');
+        $o3 = $client->testInitXunSearch('post');
+
+        self::assertTrue($o1 === $o2);
+        self::assertFalse($o2 === $o3);
+    }
+}
+
+class TestXunSearchClient extends XunSearchClient
+{
+    public function testInitXunSearch(string $indexName)
+    {
+        return $this->initXunSearch($indexName);
+    }
+}
---
+++ tests/Units/XunSearchEngineTest.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace Tests\Units;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Config;
+use JimChen\LaravelScout\XunSearch\Engines\XunSearchEngine;
+use JimChen\LaravelScout\XunSearch\XunSearchClient as SearchClient;
+use Laravel\Scout\Builder;
+use Mockery as m;
+use stdClass;
+use Tests\Fixtures\EmptySearchableModel;
+use Tests\Fixtures\SearchableModel;
+use Tests\Fixtures\SoftDeletedEmptySearchableModel;
+use Tests\TestCase;
+use XSDocument;
+
+class XunSearchEngineTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        Config::shouldReceive('get')->with('scout.after_commit', m::any())->andReturn(false);
+    }
+
+    protected function tearDown(): void
+    {
+        m::close();
+    }
+
+    public function test_update_adds_objects_to_index()
+    {
+        $client = m::mock(SearchClient::class);
+        $client->shouldReceive('initIndex')->with('table')->andReturn($index = m::mock(stdClass::class));
+        $index->shouldReceive('openBuffer', 'closeBuffer', 'flushIndex')->withNoArgs();
+        $index->shouldReceive('update')->with([
+            new XSDocument([
+                'id' => 1,
+            ])
+        ]);
+
+        $engine = new XunSearchEngine($client);
+        $engine->update(Collection::make([new SearchableModel]));
+
+        self::assertTrue(true);
+    }
+
+    public function test_delete_removes_objects_to_index()
+    {
+        $client = m::mock(SearchClient::class);
+        $client->shouldReceive('initIndex')->with('table')->andReturn($index = m::mock(stdClass::class));
+        $index->shouldReceive('del')->with([1]);
+        $index->shouldReceive('flushIndex')->withNoArgs();
+        $engine = new XunSearchEngine($client);
+        $engine->delete(Collection::make([new SearchableModel(['id' => 1])]));
+
+        self::assertTrue(true);
+    }
+
+    public function test_search_sends_correct_parameters_to_xunsearch()
+    {
+        $client = m::mock(SearchClient::class);
+        $client->shouldReceive('initSearch')->with('table')->andReturn($search = m::mock(stdClass::class));
+        $client->shouldReceive('participle')->with('table', 'zonda')->andReturn(['zonda']);
+        $client->shouldReceive('buildSearchQuery')->with(m::any(), false)->andReturn('zonda');
+        $search->shouldReceive('search')->withNoArgs()->andReturn([1,2,3]);
+        $search->shouldReceive('getLastCount')->withNoArgs()->andReturn(3);
+        $search->shouldReceive('setQuery')->with('zonda')->andReturnSelf();
+        $search->shouldReceive('setLimit')->withAnyArgs()->andReturnSelf();
+        $search->shouldReceive('addRange')->with('foo', 1, 1)->andReturnSelf();
+
+        $engine = new XunSearchEngine($client);
+        $builder = new Builder(new SearchableModel, 'zonda');
+        $builder->where('foo', 1);
+        self::assertEquals([
+            'hits' => [1, 2, 3],
+            'nbHits' => 3,
+            'facets' => collect([])
+        ], $engine->search($builder));
+    }
+
+    public function test_map_correctly_maps_results_to_models()
+    {
+        $client = m::mock(SearchClient::class);
+        $engine = new XunSearchEngine($client);
+
+        $model = m::mock(stdClass::class);
+        $model->shouldReceive('getKeyName')->andReturn('id');
+        $model->shouldReceive('getScoutModelsByIds')->andReturn($models = Collection::make([
+            new SearchableModel(['id' => 1]),
+        ]));
+
+        $builder = m::mock(Builder::class);
+
+        $results = $engine->map($builder, ['nbHits' => 1, 'hits' => [
+            ['id' => 1],
+        ]], $model);
+
+        self::assertCount(1, $results);
+    }
+
+    public function test_map_method_respects_order()
+    {
+        $client = m::mock(SearchClient::class);
+        $engine = new XunSearchEngine($client);
+
+        $model = m::mock(stdClass::class);
+        $model->shouldReceive('getKeyName')->andReturn('id');
+        $model->shouldReceive('getScoutModelsByIds')->andReturn($models = Collection::make([
+            new SearchableModel(['id' => 1]),
+            new SearchableModel(['id' => 2]),
+            new SearchableModel(['id' => 3]),
+            new SearchableModel(['id' => 4]),
+        ]));
+
+        $builder = m::mock(Builder::class);
+
+        $results = $engine->map($builder, ['nbHits' => 4, 'hits' => [
+            ['id' => 1],
+            ['id' => 2],
+            ['id' => 4],
+            ['id' => 3],
+        ]], $model);
+
+        self::assertCount(4, $results);
+
+        // It's important we assert with array keys to ensure
+        // they have been reset after sorting.
+        self::assertEquals([
+            0 => ['id' => 1],
+            1 => ['id' => 2],
+            2 => ['id' => 4],
+            3 => ['id' => 3],
+        ], $results->toArray());
+    }
+
+    public function test_update_empty_searchable_array_does_not_add_objects_to_index()
+    {
+        $client = m::mock(SearchClient::class);
+        $client->shouldReceive('initIndex')->with('table')->andReturn($index = m::mock(stdClass::class));
+        $index->shouldReceive('openBuffer', 'closeBuffer', 'flushIndex')->withNoArgs();
+        $index->shouldNotReceive('update');
+
+        $engine = new XunSearchEngine($client);
+        $engine->update(Collection::make([new EmptySearchableModel]));
+
+        self::assertTrue(true);
+    }
+
+    public function test_update_empty_searchable_array_from_soft_deleted_model_does_not_add_objects_to_index()
+    {
+        $client = m::mock(SearchClient::class);
+        $client->shouldReceive('initIndex')->with('table')->andReturn($index = m::mock('StdClass'));
+        $index->shouldReceive('openBuffer', 'closeBuffer', 'flushIndex')->withNoArgs();
+        $index->shouldNotReceive('update');
+
+        $engine = new XunSearchEngine($client, true);
+        $engine->update(Collection::make([new SoftDeletedEmptySearchableModel]));
+
+        self::assertTrue(true);
+    }
+}
